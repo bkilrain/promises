e4528f38dad3b06488bf1ed50599e416e6e543db
bkilrain <bkilrain@gmail.com>
2016-08-17 18:49:43 -0700

    Implement first line function in Callback Review

--- exercises/bare_minimum/callbackReview.js
+++ exercises/bare_minimum/callbackReview.js
@@ -6,13 +6,25 @@ var fs = require('fs');
 var request = require('request');
 
 // This function should retrieve the first line of the file at `filePath`
-var pluckFirstLineFromFile = function (filePath) {
-  // TODO
+var pluckFirstLineFromFile = function (filePath, callback) {
+
+  fs.readFile(filePath, 'utf8', function(err, data) {
+    if (err) {
+      callback(err);
+    } else {
+      var firstLine = data.split('\n')[0];
+      callback(err, firstLine);
+    }
+     
+    
+  });
 };
 
 // This function should retrieve the status code of a GET request to `url`
 var getStatusCode = function (url, callback) {
-  // TODO
+
+  callback();
+
 };
 
 // Export these functions so we can test them and reuse them in later exercises
